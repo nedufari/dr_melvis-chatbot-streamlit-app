6bc4aca66c1406de9d70b767da48fa6d1f69ed63
nedu <nedufari@gmail.com>
2023-01-04 15:00:19 +0100

    the initial commit for this project

---
+++ app.py
@@ -0,0 +1,90 @@
+import openai
+import streamlit as st
+import requests
+import json
+from streamlit_chat import message
+
+# Set your API key
+openai.api_key ='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'
+
+
+
+
+
+def generate_response(prompt):
+    # Set the API endpoint
+    api_endpoint = "https://api.openai.com/v1/completions"
+    api_key = openai.api_key
+    
+    # Set the headers for the request
+    headers = {
+        "Content-Type": "application/json",
+        "Authorization": f"Bearer {api_key}"
+    }
+    
+    # Set the payload for the request
+    data = {
+        "model":"text-davinci-003",
+        "prompt": prompt,
+        "max_tokens": 1024,
+        "temperature": 0.5,
+        "top_p": 1,
+        "frequency_penalty": 0,
+        "presence_penalty": 0,
+        "top_p":1,
+        "frequency_penalty":0
+    }
+    
+    # Send the request
+    response = requests.post(api_endpoint, headers=headers, json=data)
+    
+    # Check the status code of the response
+    if response.status_code == 200:
+        # Parse the response
+        response_json = response.json()
+        # Get the completion from the response
+        completion = response_json["choices"][0]["text"]
+        return completion
+    else:
+        # Return the error message
+        return response.text
+
+
+
+st.title("Dr Melvis Health Diagnostic Chatbot")
+
+DEFAULT_RESPONSE = "Hello, my name is Dr. Melvis, i am a health diagnonisgic chatbot bult by Anolue Francis and Zainab. i am not perfect yet, but trust i be of the best service to you in any form i can. ?"
+
+# Display the default response in the chat interface
+message(DEFAULT_RESPONSE, key='message_default')
+
+# Storing the chat
+if "generated" not in st.session_state:
+    st.session_state["generated"] = []
+
+if "past" not in st.session_state:
+    st.session_state["past"] = []
+
+# Get user input
+
+
+st.empty()
+def get_text():
+    input_text = st.text_input("patient", "Hello doctor", key="input")
+    return input_text
+
+user_input = get_text()
+
+
+if user_input:
+    output = generate_response(user_input)
+    # Store the output
+    st.session_state["past"].append(user_input)
+    st.session_state["generated"].append(output)
+    st.empty()
+
+if st.session_state["generated"]:
+    for i in range(len(st.session_state["generated"]) - 1, -1, -1):
+        message(st.session_state["generated"][i], key='message'+str(i))
+        message(st.session_state["past"][i], is_user=True, key=str(i) + "user")
+
